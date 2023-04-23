ae6df7821d6db6effa29f07ec1bd2bef1d2fa055
sekihan02 <sekihan0291@gmail.com>
2023-04-24 07:09:18 +0900

    表記変更

--- tutorial/002_aci_slack_hello/slackbot.py
+++ tutorial/002_aci_slack_hello/slackbot.py
@@ -5,7 +5,7 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # 適宜実行したいOAuth Tokens for Your WorkspaceのTokenを設定
-slack_api_key = 'xoxb-5088505216708-5176987630064-i1TVdXZn2CqzIgMmmlXbRcdg'
+slack_api_key = 'SLACK_API'
 slack_api_channel = '#general'
 def SendToSlackMessage(message):
     client = WebClient(token=slack_api_key) 
